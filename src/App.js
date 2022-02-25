78a60a389cfbae6e0f537ee98ff5d2dfdcc188c5
Mackenzel <mackenzielyarnall@gmail.com>
2022-02-25 16:05:22 -0500

    Centered Header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header contianer-fluid">
+        <h1 className="search-header">Dictionary Search</h1>
+      </header>
     </div>
   );
 }
