6cf165c18650d5d8d3033d8c759da963662b1c66
Mackenzel <mackenzielyarnall@gmail.com>
2022-02-25 15:41:35 -0500

    Created Folder/ Started React App

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello</header>
+    </div>
+  );
+}
