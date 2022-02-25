93024935ff6f30122adb7ff468237f979b13a8fc
Mackenzel <mackenzielyarnall@gmail.com>
2022-02-25 16:26:22 -0500

    Saving & alerting input submission

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header contianer-fluid">
+      <header className="App-header contianer">
         <h1 className="search-header">Dictionary Search</h1>
       </header>
+      <Dictionary />
+      <footer className="search-footer">Coded by Mackenzie Yarnall</footer>
     </div>
   );
 }
