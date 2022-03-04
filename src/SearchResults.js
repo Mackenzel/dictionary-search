e773a5a3a86ec3a4cdadba8313234378e061973a
Mackenzel <mackenzielyarnall@gmail.com>
2022-03-03 20:04:57 -0500

    Styling for Phonetics

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -9,13 +9,15 @@ export default function SearchResults(props) {
       <div className="SearchResults">
         <section className="shadow">
           <h2>{props.results.word}</h2>
-          {props.results.phonetics.map(function (phonetic, index) {
-            return (
-              <div key={index}>
-                <Phonetic phonetic={phonetic} />
-              </div>
-            );
-          })}
+          <div className="row ps-4">
+            {props.results.phonetics.map(function (phonetic, index) {
+              return (
+                <div className="col" key={index}>
+                  <Phonetic phonetic={phonetic} />
+                </div>
+              );
+            })}
+          </div>
         </section>
         {props.results.meanings.map(function (meaning, index) {
           return (
